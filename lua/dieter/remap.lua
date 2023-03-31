a8885e3f3601ab4ce9d3293149f603cca2db2902
Dieter <dieterhaag@yxcvb.net>
2023-03-31 19:03:27 +0200

    remap auf Neotree

--- lua/dieter/remap.lua
+++ lua/dieter/remap.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---vim.keymap.set("n", "<leader>pv", Neotree)
 
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Neotree)
+
+--vim.api.nvim_set_keymap("n", "<leader>pv", "Neotree", { silent = true })
 -- fuer unimpaired
 vim.api.nvim_set_keymap("n", "<", "[", { silent = true })
 vim.api.nvim_set_keymap("n", ">", "]", { silent = true })
