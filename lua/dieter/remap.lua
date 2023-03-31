82f319fd459d2f9c33e9b305f487fc7a2f06312a
Dieter <dieterhaag@yxcvb.net>
2023-03-31 20:44:28 +0200

    undotree & UserProfile

--- lua/dieter/remap.lua
+++ lua/dieter/remap.lua
@@ -1,7 +1,11 @@
 vim.g.mapleader = " "
 
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Filebrowser
 vim.keymap.set("n", "<leader>pv", vim.cmd.Neotree)
+
+-- undotree
+vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle)
 
 --vim.api.nvim_set_keymap("n", "<leader>pv", "Neotree", { silent = true })
 -- fuer unimpaired
