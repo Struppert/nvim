2bbbad8b3c41e00e5d648a652d26bc24a072d868
Dieter <dieterhaag@yxcvb.net>
2024-04-11 00:14:02 +0200

    autocommands for parsen in msvc errors

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.commentstring = "// %s"
   end,
 })
+
+-- Die nächsten Zeilen sollen dafür sorgen, dass Fehlermeldungen im Quickfix richtig geparst werden.
+vim.api.nvim_command("autocmd BufRead,BufNewFile *.sln set filetype=msbuild")
+vim.api.nvim_command("autocmd BufRead,BufNewFile *.vcxproj set filetype=msbuild")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "msbuild", -- Muster, das mit dem Dateityp übereinstimmen soll
+  callback = function()
+    vim.opt_local.errorformat = '%A%*[%t[^]]%*]%*[ ]%f(%l\\,%c):%m,"%Z%*\\s%\\@=%m,"%-G%.%#' -- Konfiguration des errorformats
+  end,
+})
