6c1ef405f6475bdab772c16735512ec904a99344
Dieter Haag <dh@leutek.de>
2024-04-24 08:17:41 +0200

    disable bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,4 +1,10 @@
 return {
-  -- disable bufferline
-  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+    opts = function(_, opts)
+      opts.options.show_buffer_close_icons = false
+      opts.options.show_close_icon = false
+    end,
+  },
 }
